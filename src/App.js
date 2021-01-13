43ef794fd326a0dce409d0ec48efaa28d7f5ac7d
lisaau <lisaau@users.noreply.github.com>
2021-01-13 10:57:52 -0800

    Display data in table format

--- src/App.js
+++ src/App.js
@@ -2,9 +2,66 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-    </div>
+    <table>
+        <thead>
+          <th></th>
+          <th>Name</th>
+          <th>Device</th>
+          <th>Path</th>
+          <th>Status</th>
+        </thead>
+        <tbody>
+          {data.map((item) => {
+            return (
+              <tr>
+                <td>
+                  <input type="checkbox" />
+                </td>
+                <td>{item.name}</td>
+                <td>{item.device}</td>
+                <td>{item.path}</td>
+                <td>{item.status}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
   );
+
+
 }
+
+const data = [
+  {
+    name: "smss.exe",
+    device: "Stark",
+    path: "\\Device\\HarddiskVolume2\\Windows\\System32\\smss.exe",
+    status: "scheduled"
+  },
+  {
+    name: "netsh.exe",
+    device: "Targaryen",
+    path: "\\Device\\HarddiskVolume2\\Windows\\System32\\netsh.exe",
+    status: "available"
+  },
+  {
+    name: "uxtheme.dll",
+    device: "Lanniester",
+    path: "\\Device\\HarddiskVolume1\\Windows\\System32\\uxtheme.dll",
+    status: "available"
+  },
+  {
+    name: "cryptbase.dll",
+    device: "Martell",
+    path: "\\Device\\HarddiskVolume1\\Windows\\System32\\cryptbase.dll",
+    status: "scheduled"
+  },
+  {
+    name: "7za.exe",
+    device: "Baratheon",
+    path: "\\Device\\HarddiskVolume1\\temp\\7za.exe",
+    status: "scheduled"
+  }
+];
 
 export default App;
